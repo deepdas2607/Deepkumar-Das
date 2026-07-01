e20f45087d1a87e28df6cfdc854f77473a78a6c4
Deepkumar Das <157793903+deepdas2607@users.noreply.github.com>
2026-07-01 12:32:38 +0530

    Add files via upload

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
             <Float>
               <Astronaut
                 scale={isMobile ? 0.21 : 0.35}
-                position={isMobile ? [0, -1.5, 0] : [3, -1.5, 0]}
+                position={isMobile ? [0, -1.5, 0] : [2.2, -1.5, 0]}
               />
             </Float>
             <Rig />
